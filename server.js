fbcd888c587d218c9fe33857bb7b159d9d1c49b0
Piorozla <piorozla@o2.pl>
2018-01-19 21:10:53 +0000

    Added default route

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require('express');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Blank');
+});
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
